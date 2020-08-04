7567b093f0254f65222887ec580ce278d173b860
Cristian-byte <65248632+Cristian-byte@users.noreply.github.com>
2020-08-04 18:16:06 -0500

    crea archivos iniciales del proyecto

--- index.php
+++ index.php
@@ -81,17 +81,17 @@
             <div class="contenedor">
                 <h3 class="titulo">Ubicacion</h3>
                     <div class="direccion">
-                        <p class="calle">Emiliano Zapata <br> Xaloztoc, Tlaxcala 90460</p>
-                        <p class="telefono">(241) 789-4651</p>
-                        <p class="correo">correo[arroba]tucorreo.com</p>
+                        <p class="calle">Calle Iturbide s/n <br> Cuapiaxtla, Tlaxcala 90560</p>
                     </div>
 
                     <div class="horarios">
                         <h4>Horarios</h4>
-                        <p class="entre-semana">Lunes a Viernes <br> 8:00am - 8:00pm</p>
-                        <p class="fin-semana">Sabado y Domingo <br> 9:00am - 3:00pm</p>
+                        <p class="entre-semana">Lunes a Viernes <br> 8:00am - 4:00pm</p>
                     </div>
             </div>
+            <section class="mapa">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.6997819771195!2d-97.77067178514524!3d19.295417586962454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85c55612035bb3eb%3A0x93d48eef8f70ee28!2sCalle%20Agust%C3%ADn%20de%20Iturbide%2C%20San%20Miguel%2C%2090560%20Cuapiaxtla%2C%20Tlax.!5e0!3m2!1ses-419!2smx!4v1596582264934!5m2!1ses-419!2smx" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </section>
         </section>
           <div class="col-lg-4 col-md-4 col-sm-6 ">
             <div class="widget">
