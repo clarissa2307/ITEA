fec3bcd00551277b91419779ba8436113ae7a736
Cristian-byte <65248632+Cristian-byte@users.noreply.github.com>
2020-08-03 21:05:42 -0500

    crea archivos iniciales del proyecto

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITEA</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <nav class="menu">
+                <img src="img/ITEA.jpg" alt="" class="card-img-top">   
+                <img src="img/tlx.jpg" alt="" class="card-img-top">
+                <a href="#" id="btn-acerca-de">Acerca de</a>
+                <a href="#" id="btn-servicios">Servicios</a>
+                <a href="#" id="btn-ubicacion">Ubicacion</a>
+            </nav>
+            <div class="textos">
+                <h1 class="nombre">ITEA</h1>
+                <h3>Instituto Tlaxcalteca para la Educacion de los Adultos</h3>
+            </div>
+        </div>
+    </header>
+
+    <section class="main">
+        <section class="acerca-de" id="acerca-de">
+            <div class="contenedor">
+                <div class="foto">
+                    <img src="img/acerca-de.jpg" alt="">
+                </div>
+                <article>
+                    <h3>Acerca de</h3>
+                    <p>Somos una Institución encargada de ofrecer, Servicios Educativos de calidad a los jóvenes y adultos que presentan rezago educativo, en materia de educación básica, proporcionándoles herramientas para su crecimiento y desarrollo personal.</p> 
+                </article>
+            </div>
+        </section>
+
+    <main class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="py-4 border-bottom">
+                    <h1 class="text-center">Servicios</h1>
+                </div>
+            </div>
+        </div>
+        <div class="row py-4">
+            <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="img/alfa.jpg" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h3 class="card-title">Alfabetización</h3>
+                        <p class="card-text">CURP </br> 15 años cumplidos. </br> Fotografia digital tamaño infantil. </br> Llenar el formato.</p>
+                        <a href="#" class="btn btn-sm btn-secondary">Registrarse</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="img/primaria.jpg" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h3 class="card-title">Primaria</h3>
+                        <p class="card-text">CURP </br> 15 años cumplidos. </br> Fotografia digital tamaño infantil. </br> Boletas de ultimo grado de estudios </br> Llenar el formato.</p>
+                        <a href="#" class="btn btn-sm btn-secondary">Registrarse</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="img/secundaria.jpg" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h3 class="card-title">Secundaria</h3>
+                        <p class="card-text">CURP </br> 15 años cumplidos. </br> Acta de nacimiento. </br> Boletas de primero y/o segundo grado. </br> Fotografia digital tamaño infantil. </br> Boletas de primero y/o segundo grado de secundaria </br> Llenar el formato.</p>
+                        <a href="#" class="btn btn-sm btn-secondary">Registrarse</a>
+                    </div>
+                </div>
+            </div>
+    </section>
+    
+    <footer>
+        <section class="ubicacion" id="ubicacion">
+            <div class="contenedor">
+                <h3 class="titulo">Ubicacion</h3>
+                    <div class="direccion">
+                        <p class="calle">Emiliano Zapata <br> Xaloztoc, Tlaxcala 90460</p>
+                        <p class="telefono">(241) 789-4651</p>
+                        <p class="correo">correo[arroba]tucorreo.com</p>
+                    </div>
+
+                    <div class="horarios">
+                        <h4>Horarios</h4>
+                        <p class="entre-semana">Lunes a Viernes <br> 8:00am - 8:00pm</p>
+                        <p class="fin-semana">Sabado y Domingo <br> 9:00am - 3:00pm</p>
+                    </div>
+            </div>
+        </section>
+          <div class="col-lg-4 col-md-4 col-sm-6 ">
+            <div class="widget">
+              <h5 class="widgetheading">Pre-registro</h5>
+              <p>
+                Registrate en los Servicios que Ofrece el ITEA, como Educando y Figura Operativa.
+              </p>
+              <form class="subscribe">
+                <div class="input-append">
+                  <a href="apps/pre-registro" target="_black">
+                    <button class="btn btn-theme" type="button">Registrar</button>
+                  </a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="sub-footer">
+        <div class="containeer">
+          <div class="roow">
+            <div class="col-lg-6 col-sm-12">
+              <div class="copyright">
+                <p><span>&copy; ITEA Tlaxcala. Dirección de Informática</span></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
