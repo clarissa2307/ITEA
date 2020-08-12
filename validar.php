4ad0870a458cb9e53056c55255bb8cb41272beed
clarissa2307 <marthaleo07@gmail.com>
2020-08-12 14:01:04 -0500

    d

--- validar.php
+++ validar.php
@@ -14,10 +14,10 @@ session_start();
 	//la variable  $mysqli viene de connect_db que lo traigo con el require("connect_db.php");
 	$sql2=mysqli_query($mysqli,"SELECT * FROM usuario WHERE correo='$username'");
 	if($f2=mysqli_fetch_assoc($sql2)){
-		if($pass==$f2['pass']){
-			$_SESSION['id_usuario']=$f2['id_usuario'];
+		if($pass==$f2['passu']){
+			//$_SESSION['id_usuario']=$f2['id_usuario'];
 			$_SESSION['correo']=$f2['correo'];
-			$_SESSION['rol']=$f2['rol'];
+			$_SESSION['rol']=$f2['1'];
 
 			echo '<script>alert("BIENVENIDO ADMINISTRADOR")</script> ';
 			echo "<script>location.href='admin.php'</script>";
@@ -28,10 +28,10 @@ session_start();
 
 	$sql=mysqli_query($mysqli,"SELECT * FROM usuario WHERE correo='$username'");
 	if($f=mysqli_fetch_assoc($sql)){
-		if($pass==$f['passu']){
-			$_SESSION['id_usuario']=$f['id_usuario'];
+		if($pass==$f['pass']){
+			//$_SESSION['id_usuario']=$f['id_usuario'];
 			$_SESSION['correo']=$f['correo'];
-			$_SESSION['rol']=$f['rol'];
+			$_SESSION['rol']=$f['2'];
 
 			header("Location: formularioedu.php");
 		}else{
