d922d15ae0aa716afa6f4ada8b3d1025eeedff8e
Cristian-byte <65248632+Cristian-byte@users.noreply.github.com>
2020-08-12 21:35:32 -0500

    crea archivos iniciales

--- connect_db.php
+++ connect_db.php
@@ -3,7 +3,7 @@
 
 		$mysqli = new MySQLi("localhost", "root","", "itea");
 		if ($mysqli -> connect_errno) {
-			die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
+			die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_error();
 				. ") " . $mysqli -> mysqli_connect_error());
 		}
 		else
