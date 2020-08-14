578a5f09a52a0f2ff851372d3e6d382114f366e0
clarissa2307 <marthaleo07@gmail.com>
2020-08-13 21:22:23 -0500

    LIS

--- formularioedu.php
+++ formularioedu.php
@@ -38,29 +38,29 @@
       <div class="form-row">
         <div class="col-md-4 mb-3">
           <label>Nombre</label>
-          <input type="text" placeholder="Nombre" class="form-control is-valid" name="nombre" value="<?php echo $nombre?>" required>
+          <input type="text" placeholder="Nombre" class="form-control " name="nombre" value="" required>
         </div>
         <div class="col-md-4 mb-3">
           <label for="">Apellido Paterno</label>
-          <input type="text" placeholder="Apellido Paterno" class="form-control is-valid" name="apellidop"  value="<?php echo $apellidop?>" required>
+          <input type="text" placeholder="Apellido Paterno" class="form-control " name="apellidop"  value="" required>
         </div>
         <div class="col-md-4 mb-3">
           <label >Apellido Materno</label>
-          <input type="text" placeholder="Apellido Materno" class="form-control is-valid"  name="apellidom"  value="<?php echo $apellidom?>" required>
+          <input type="text" placeholder="Apellido Materno" class="form-control "  name="apellidom"  value="" required>
         </div>
       </div>
       <div class="form-row">
         <div class="col-md-6 mb-3">
           <label >Curp</label>
-          <input type="text" placeholder="Curp" class="form-control is-valid" name="curp"  value="<?php echo $curp?>" required>
+          <input type="text" placeholder="Curp" class="form-control " name="curp"  value="" required>
         </div>
         <div class="col-md-3 mb-3">
           <label >Edad</label>
-          <input type="number" placeholder="Edad" class="form-control"  name="edad"  value="<?php echo $edad?>" required>
+          <input type="number" placeholder="Edad" class="form-control"  name="edad"  value="" required>
         </div>
         <div class="col-md-3 mb-3">
           <label >Genero</label>
-          <select class="custom-select"  name="genero"  value="<?php echo $genero?>" required>
+          <select class="custom-select"  name="genero"  value="" required>
             <option>Mujer</option>
             <option>Hombre</option>
           </select>
@@ -69,11 +69,11 @@
       <div class="form-row">
         <div class="col-md-3 mb-3">
           <label >Fecha Nacimiento</label>
-          <input type="date" placeholder="Edad" class="form-control " name="fecha"  value="<?php echo $fecha?>" required>
+          <input type="date" placeholder="Edad" class="form-control " name="fecha"  value="" required>
         </div>
         <div class="col-md-4 mb-3">
           <label>Correo electronico</label>
-          <input type="email" placeholder="Correo electronico" class="form-control is-valid" name="correo"  value="<?php echo $correo?>" required>
+          <input type="email" placeholder="Correo electronico" class="form-control " name="correo"  value="" required>
         </div>
       </div>  
       
@@ -83,35 +83,35 @@
       <div class="form-row">
         <div class="col-md-6 mb-3">
           <label >Domicilio</label>
-          <input type="text" placeholder="Nombre Domicilio" class="form-control is-valid" name="calle"  value="<?php echo $calle?>" required>
+          <input type="text" placeholder="Nombre Domicilio" class="form-control " name="calle"  value="" required>
         </div>
         <div class="col-md-3 mb-3">
           <label >No. Ext.</label>
-          <input type="text" placeholder="No. Ext." class="form-control is-valid"  name="ext" value="<?php echo $ext?>" required>
+          <input type="text" placeholder="No. Ext." class="form-control "  name="ext" value="" required>
         </div>
         <div class="col-md-3 mb-3">
           <label >No. Int.</label>
-          <input type="text" placeholder="No. Int" class="form-control is-valid" name="inte"  value="<?php echo $inte?>" required>
+          <input type="text" placeholder="No. Int" class="form-control " name="inte"  value="" required>
         </div>
       </div>
       <div class="form-row">
         <div class="col-md-4 mb-3">
           <label >Municipio</label>
-          <input type="text" placeholder="Municipio" class="form-control is-valid" name="municipio" value="<?php echo $municipio?>" required>
+          <input type="text" placeholder="Municipio" class="form-control " name="municipio" value="" required>
         </div>
         <div class="col-md-4 mb-3">
           <label >Localidad</label>
-          <input type="text" placeholder="Localidad" class="form-control is-valid" name="localidad"  value="<?php echo $localidad?>" required>
+          <input type="text" placeholder="Localidad" class="form-control " name="localidad"  value="" required>
         </div>
         <div class="col-md-4 mb-3">
           <label >Colonia</label>
-          <input type="text" placeholder="Colonia" class="form-control is-valid" name="colonia"  value="<?php echo $colonia?>" required>
+          <input type="text" placeholder="Colonia" class="form-control " name="colonia"  value="" required>
         </div>
       </div>
       <div class="form-row">
         <div class="col-md-4 mb-3">
           <label >Telefono Celular</label>
-          <input type="tel" placeholder="Telefono Celular" class="form-control is-valid" name="telefono"  value="<?php echo $telefono?>" required>
+          <input type="tel" placeholder="Telefono Celular" class="form-control " name="telefono"  value="" required>
         </div>
         
         </div>
@@ -121,19 +121,14 @@
       <div class="form-row">
         <div class="col-md-3 mb-3">
           <label >Nivel que solicita</label>
-          <select class="custom-select"  name="estudio" value="<?php echo $estudio?>" required>
+          <select class="custom-select"  name="estudio" value="" required>
             <option>Alfabetizacion</option>
             <option>Primaria</option>
             <option>Secundaria</option>
           </select>
         </div>
       </div>
-	    <div class="form-row">
-	    <div class="col-md-4 mb-3">
-          
-          <input type="text" placeholder="ID" class="form-control " name="id_educando" value="<?php echo $id_educando?>" hidden>
-        </div>
-      
+	    
       <div class="form-group">
         <button type="submit" class="btn btn-primary" name="submit">Enviar</button>
       </div>
